c264913971bf70f1a7306a13220cc729841cb365
Francois-Xavier Rolland <rolland.fx@gmail.com>
2017-04-17 20:24:40 -0400

    Commentaires basiques et cleanage
    self descript

--- exec-precalc.py
+++ exec-precalc.py
@@ -1,3 +1,13 @@
+#
+# Ceci n'est QUE UN PROOF OF CONCEPT !
+# le code est moche (je le sais)
+# principe : si modele deja dispo, lancer la validation
+# sinon, iterrer de multiples fois en apportant des modifications au photos sur le batch de photo de training
+# puis lancer la vérification
+#
+# tout les paramtres sont purement configuré au feeling, c'est sans aucun doute pas optimisé
+#
+# probablement trop d'include, mais la...ca marche !
 from keras.preprocessing.image import ImageDataGenerator , array_to_img, img_to_array, load_img
 from keras.models import Sequential , model_from_json
 from keras.layers import Conv2D, MaxPooling2D , Convolution2D, MaxPooling2D, ZeroPadding2D, Dropout, Flatten, Dense
@@ -13,9 +23,10 @@ import time
 from pathlib import Path
 
 
-# dimensions of our images.
+# dimensions des images apres resize (le training ne resize pas automatiquement)
 img_width, img_height = 1000, 664
 
+# epochs et batch_size sont des parametres qu'il faudra optimiser
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
 test_data_dir = 'data/test'
@@ -25,12 +36,12 @@ nb_validation_samples = 158
 nb_test_samples = 8
 epochs = 70
 batch_size = 7
-
+# pour permettre une repetabilité du training
 np.random.seed(100)
-
+# detection de non variance dans le training =» modele assez ou trop optimisé =» stop
 early_stopping = EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto')
 
-
+#fonction de classement des images avec un modele existant
 def predict_labels(model):
     """writes test image labels and predictions to csv"""
     base_path = "data/test/"
@@ -50,7 +61,7 @@ def predict_labels(model):
             for im in imgs:
                 pic_id = im.split(".")[0]
                 img = load_img(base_path + im)
-                img = imresize(img, size=(img_height, img_width))
+                img = imresize(img, size=(img_height, img_width)) #resize des images a tester
                 test_x = img_to_array(img).reshape(3, img_width,  img_height)
                 test_x = test_x.reshape((1,) + test_x.shape)
                 test_generator = test_datagen.flow(test_x,
@@ -99,15 +110,14 @@ if __name__ == "__main__":
                       optimizer='rmsprop',
                       metrics=['accuracy'])
 
-        # this is the augmentation configuration we will use for training
+        # on tranforme les images aléatoirement a chaque passage pour "créer" virtuellement plus d'imagee
         train_datagen = ImageDataGenerator(
             rescale=1. / 255,
             shear_range=0.2,
-            #zoom_range=0.2,
+            #zoom_range=0.2, # le zoom sort certaines cibles des images, rendant le training moins bon
             horizontal_flip=True)
 
-        # this is the augmentation configuration we will use for testing:
-        # only rescaling
+        # on rescale pour le training
         test_datagen = ImageDataGenerator(rescale=1. / 255)
 
         train_generator = train_datagen.flow_from_directory(
